0d50e2ee950d7f1088d202df281e8e47fbdcbbd6
JenSeop <dostwoman@gmail.com>
2023-06-26 21:38:43 +0900

    [23.06.26] test cbv & api

--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from .fbv_views import HelloAPI, bookAPI, booksAPI
+from .cbv_views import BookAPI, BooksAPI
 
 urlpatterns = [
     path("hello/", HelloAPI),
+    # FBV
     path("fbv/books/", booksAPI),
     path("fbv/book/<int:bid>/", bookAPI),
+    # CBV
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
 ]
